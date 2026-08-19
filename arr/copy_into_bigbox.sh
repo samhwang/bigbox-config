56866d8005d8e16f9d1de72abece49ae94069b8e
Sam Huynh <samhwang2112.dev@gmail.com>
2026-08-19 22:25:10 +1000

    arr: fix copy script destination

--- arr/copy_into_bigbox.sh
+++ arr/copy_into_bigbox.sh
@@ -5,6 +5,6 @@
 # - ssh config created on host machine
 
 echo "transferring config files into bigbox"
-rsync -rzvhP ./config/traefik samhwang@bigbox:/mnt/vault/arr2/config/traefik
-rsync -rzvhP ./config/decluttarr samhwang@bigbox:/mnt/vault/arr2/config/decluttarr
+rsync -rzvhP ./config/traefik samhwang@bigbox:/mnt/vault/arr2/config
+rsync -rzvhP ./config/decluttarr samhwang@bigbox:/mnt/vault/arr2/config
 rsync -zvhP ./docker-compose.yaml samhwang@bigbox:/mnt/vault/arr2/docker-compose.yaml
